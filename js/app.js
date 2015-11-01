8cc347218fb100af76c6098f03bc174763e4c43e
Jake <Jake@Jakes-MacBook-Pro.local>
2015-11-01 00:14:44 +0000

    Initial styling
    font styles
    nav function
    hover states

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// icon flipping
+$(document).ready(function(){
+	$('.icon-flip').click(function(){
+		$(this).toggleClass('flipped')
+	})
+	$('#topnav_button, #topnav .nav_item').click(function(){
+		$('#topnav').fadeToggle(400)
+	})
+})
